07499b9eb4a3bf9cf9787fe2d786018814aaccf0
Muryllo Perim Dev <murylloperimdev@gmail.com>
2025-01-08 19:05:38 -0300

    Arrumando botao reiniciar

--- app.js
+++ app.js
@@ -46,12 +46,11 @@ function alterarStatusBotao() {
         botao.classList.remove('container__botao');
         botao.classList.add('container__botao-desabilitado');
     }
-
-    function reiniciar() {
+}
+    function reiniciar () {
         document.getElementById('quantidade').value = '';
         document.getElementById('de').value = '';
         document.getElementById('ate').value = '';
         document.getElementById('resultado').innerHTML = `<label class="texto__paragrafo">Números sorteados: </label>`;
         alterarStatusBotao();
     }
-}
